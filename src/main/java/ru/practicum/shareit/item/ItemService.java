9b7281b090fdc73e99544b5aaac3e1a2cc50151c
Anton <info@telecomus.ru>
2025-03-07 20:23:45 +0600

    sp14-fix-style

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto create(long userId, ItemDto itemDto);
+
     ItemDto update(long userId, long itemId, ItemDto itemDto);
+
     ItemDto getById(long itemId);
+
     List<ItemDto> getAllByUserId(long userId);
+
     List<ItemDto> search(String text);
 }
--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface UserService {
     UserDto create(UserDto userDto);
+
     UserDto update(long userId, UserDto userDto);
+
     UserDto getById(long userId);
+
     List<UserDto> getAll();
+
     void delete(long userId);
 }
